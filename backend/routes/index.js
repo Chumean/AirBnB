0847c0997aeb14ef2d1ddce20d03059763f7e57d
david <david.chaotic.chu@gmail.com>
2023-01-12 00:46:10 -0500

    Add error-handling middleware for common errors and debugging

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,10 +2,15 @@
 const express = require('express');
 const router = express.Router();
 
-// router.get('/hello/world', function(req, res) {
-//   res.cookie('XSRF-TOKEN', req.csrfToken());
-//   res.send('Hello World!');
-// });
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
+
+router.get('/hello/world', function(req, res) {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.send('Hello World!');
+});
 
 router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
@@ -13,12 +18,6 @@ router.get("/api/csrf/restore", (req, res) => {
     res.status(200).json({
       'XSRF-Token': csrfToken
     });
-  });
-
-
-
-const apiRouter = require('./api');
-
-router.use('/api', apiRouter);
+});
 
 module.exports = router;

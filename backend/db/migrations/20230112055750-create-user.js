ac67cfe75b5b0e84d2573d982c4826bc7e9f267d
david <david.chaotic.chu@gmail.com>
2023-01-12 22:45:39 -0500

    Add Users table migration and seeds

---
+++ backend/db/migrations/20230112055750-create-user.js
@@ -0,0 +1,49 @@
+'use strict';
+let options = {};
+
+if(process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA; // define schema in options object
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      hashedPassword: {
+        type: Sequelize.STRING.BINARY,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Users";
+    await queryInterface.dropTable(options);
+    // await queryInterface.dropTable('Users');
+  }
+};

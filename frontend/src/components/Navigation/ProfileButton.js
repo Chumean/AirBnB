fc4dafd661054d1c12e9039f9de8d730561c6da1
david <david.chaotic.chu@gmail.com>
2023-02-25 14:49:30 -0500

    add bonus phase incomplete

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,6 +1,64 @@
+// import React, { useState, useEffect, useRef } from "react";
+// import { useDispatch } from 'react-redux';
+// import * as sessionActions from '../../store/session';
+
+// function ProfileButton({ user }) {
+//   const dispatch = useDispatch();
+//   const [showMenu, setShowMenu] = useState(false);
+//   const ulRef = useRef();
+
+//   const openMenu = () => {
+//     if (showMenu) return;
+//     setShowMenu(true);
+//   };
+
+//   useEffect(() => {
+//     if (!showMenu) return;
+
+//     const closeMenu = (e) => {
+//       if (!ulRef.current.contains(e.target)) {
+//         setShowMenu(false);
+//       }
+//     };
+
+//     document.addEventListener('click', closeMenu);
+
+//     return () => document.removeEventListener("click", closeMenu);
+//   }, [showMenu]);
+
+//   const logout = (e) => {
+//     e.preventDefault();
+//     dispatch(sessionActions.logout());
+//   };
+
+//   const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+
+//   return (
+//     <>
+//       <button onClick={openMenu}>
+//         <i className="fas fa-user-circle" />
+//       </button>
+//       <ul className={ulClassName} ref={ulRef}>
+//         <li>{user.username}</li>
+//         <li>{user.firstName} {user.lastName}</li>
+//         <li>{user.email}</li>
+//         <li>
+//           <button onClick={logout}>Log Out</button>
+//         </li>
+//       </ul>
+//     </>
+//   );
+// }
+
+// export default ProfileButton;
+
+// frontend/src/components/Navigation/ProfileButton.js
 import React, { useState, useEffect, useRef } from "react";
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
+import OpenModalButton from '../OpenModalButton';
+import LoginFormModal from '../LoginFormModal';
+import SignupFormModal from '../SignupFormModal';
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
@@ -39,12 +97,31 @@ function ProfileButton({ user }) {
         <i className="fas fa-user-circle" />
       </button>
       <ul className={ulClassName} ref={ulRef}>
-        <li>{user.username}</li>
-        <li>{user.firstName} {user.lastName}</li>
-        <li>{user.email}</li>
-        <li>
-          <button onClick={logout}>Log Out</button>
-        </li>
+        {user ? (
+          <>
+            <li>{user.username}</li>
+            <li>{user.firstName} {user.lastName}</li>
+            <li>{user.email}</li>
+            <li>
+              <button onClick={logout}>Log Out</button>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <OpenModalButton
+                buttonText="Log In"
+                modalComponent={<LoginFormModal />}
+              />
+            </li>
+            <li>
+              <OpenModalButton
+                buttonText="Sign Up"
+                modalComponent={<SignupFormModal />}
+              />
+            </li>
+          </>
+        )}
       </ul>
     </>
   );
